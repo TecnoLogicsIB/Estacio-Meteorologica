7cd98205af5d7646a49c2fa09d659c4618411b4d
wescraft69 <20wagb2512@ibellvitge.net>
2025-01-16 13:51:15 +0100

    Add files via upload

---
+++ UV_Meter.py
@@ -0,0 +1,19 @@
+from machine import Pin, ADC
+from time import sleep
+
+sensor = ADC(Pin(34))
+sensor.atten(ADC.ATTN_11DB)
+sensor.width(ADC.WIDTH_10BIT)
+
+def dades():
+    global valor,index_uv,tensio
+    valor = sensor.read()
+    tensio = valor * 3300/1023
+    
+    
+    if (tensio < 227.0):
+        index_uv = 0
+    elif (tensio >= 1170.0):
+        index_uv = 11
+    else:
+        index_uv = int((tensio-132.7)/94.3)
