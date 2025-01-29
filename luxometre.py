5b854dd49a7ea00eb0e69139432d2a27b380da46
TecnoLògics Bellvitge <tecnologics@ibellvitge.net>
2025-01-29 20:23:56 +0100

    Update luxometre.py

--- luxometre.py
+++ luxometre.py
@@ -6,7 +6,8 @@ from machine import Pin, SoftI2C
 from bh1750 import BH1750
 
 i2c = SoftI2C (scl=Pin(22), sda=Pin(21), freq=400000)
-sensor = BH1750 (bus=i2c, addr=0x23)    
+sensor = BH1750 (bus=i2c, addr=0x23)  
+lux = 0.0
 
 def recupera():
     global lux
