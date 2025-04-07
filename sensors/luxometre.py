ec959a6a47ff1cd2273b26c9a08b1d5d8ec9601c
TecnoLògics Bellvitge <tecnologics@ibellvitge.net>
2025-04-07 06:53:55 +0200

    Update luxometre.py

--- sensors/luxometre.py
+++ sensors/luxometre.py
@@ -3,7 +3,7 @@
 # creat per Alexjurjo i Bielesga
 
 from machine import Pin, SoftI2C
-from bh1750 import BH1750
+from bh1750 import BH1750  # llibreria bh1750 desada en la carpeta lib del dispositiu
 
 i2c = SoftI2C (scl=Pin(22), sda=Pin(21), freq=400000)
 sensor = BH1750 (bus=i2c, addr=0x23)  
