fdab2d024be5a7f638e14e067ceb125e0e389f37
TecnoLògics Bellvitge <tecnologics@ibellvitge.net>
2026-03-01 12:21:35 +0100

    Afegit: pujada de les dades rebudes a Thinsgpeak

--- receptor/receptor_4.py
+++ receptor/receptor_4.py
@@ -27,7 +27,7 @@ uv = 0
 
 ssid = 'xxx'
 psw = 'zzz'
-url = 'https://api.thingspeak.com/update?api_key=BK5LJ7G6KJN1FD3A'
+url = 'https://api.thingspeak.com/update?api_key=JCSNK4E7IP1DMUD7'
 
 miwifi.connecta_wifi(ssid, psw)
 
@@ -90,3 +90,4 @@ while True:
         led.value(not led_actiu)
         led_actiu = not led_actiu
         temps_led = ara 
+
