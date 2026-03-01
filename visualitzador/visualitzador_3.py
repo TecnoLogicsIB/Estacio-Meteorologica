b95123eb9b8c05aa3632e5719247f55748f38bd3
TecnoLògics Bellvitge <tecnologics@ibellvitge.net>
2026-03-01 12:30:28 +0100

    Afegit: mostra de dades en pantalles LCD

--- visualitzador/visualitzador_3.py
+++ visualitzador/visualitzador_3.py
@@ -21,7 +21,7 @@ interval = 10000
 
 ssid = 'SSID'
 psw = 'PASSWORD'
-url = 'https://api.thingspeak.com/channels/1825502/feeds.json?results=1'
+url = 'https://api.thingspeak.com/channels/2933623/feeds.json?results=1'
 
 # --- Accions inicials ---
 #lcd1.backlight_on()
@@ -72,3 +72,4 @@ while True:
         except Exception as e:
             print("Error:", e)
             
+
